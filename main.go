d938b7778660fba6071ac02f9b0a14030818d571
mohamedmedjabri@outlook.fr <mohamedmedjabri@outlook.fr>
2024-06-03 19:39:20 +0100

    bref

--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
-const port = "8080"
+type readinessHandler struct{}
+
+func (rh *readinessHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+
+}
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+
+	mux.Handle("/healthz", &readinessHandler{})
 
 	srv := &http.Server{
-		Handler: mux,
 		Addr:    ":" + port,
+		Handler: mux,
 	}
 
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	srv.ListenAndServe()
 }
