3e8b60430d518eb9169b713067af921d3204bfb2
mohamedmedjabri@outlook.fr <mohamedmedjabri@outlook.fr>
2024-06-05 16:21:25 +0100

    routing and middleware

--- main.go
+++ main.go
@@ -1,27 +1,68 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
 
+// Define the struct to hold state
+type apiConfig struct {
+	mu             sync.Mutex
+	fileserverHits int
+}
+
+// Middleware to increment the fileserverHits counter
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.mu.Lock()
+		cfg.fileserverHits++
+		cfg.mu.Unlock()
+		next.ServeHTTP(w, r) // Call the next handler
+	})
+}
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	prevCount := cfg.fileserverHits
+	cfg.mu.Lock()
+	cfg.fileserverHits = 0
+	cfg.mu.Unlock()
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits count was %d, ,now reset to 0", prevCount)))
+}
+
+// Handler to return the current hit count
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+// Handler to return OK for readiness
 type readinessHandler struct{}
 
 func (rh *readinessHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
-
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	apiCfg := &apiConfig{
+		fileserverHits: 0,
+	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	handler := http.FileServer(http.Dir(filepathRoot))
+	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(handler)))
 
-	mux.Handle("/healthz", &readinessHandler{})
+	mux.Handle("GET /api/healthz", &readinessHandler{})
+	mux.HandleFunc("/api/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/api/reset", apiCfg.resetHandler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
